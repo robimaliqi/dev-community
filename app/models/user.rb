e17efd56eff0a1eb1ea0905ebc52c3924232f46e
Robi <robi.maliqi@hotmail.co.uk>
2023-10-09 13:00:28 +0100

    Add Profile Titles in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
+
+  POFILE_TITLE = [
+    'Senior Ruby on Rails Developer',
+    'Full Stack Ruby on Rails Developer',
+    'Senior Full Stack Ruby on Rails Developer',
+    'Junior Full Stack Ruby on Rails Developer',
+    'Senior Java Developer',
+    'Junior Java Developer'
+    'Senior Front End Developer',
+    'Senior Back End Developer',
+    'Junior Front End Devevloper',
+    'Junior Back End Devevloper',
+    'Senior Devops Devevloper',
+    'Cloud Developer',
+    'AWS Developer'
+  ].freeze
 end
