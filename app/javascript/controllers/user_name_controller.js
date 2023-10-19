979a230a591728b362126fc7635de19f736b1897
Robi <robi.maliqi@hotmail.co.uk>
2023-10-19 19:51:55 +0100

    Added javascript code in user name controller to see how stimulasJS works

--- app/javascript/controllers/user_name_controller.js
+++ app/javascript/controllers/user_name_controller.js
@@ -1,7 +1,15 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="user-name"
 export default class extends Controller {
+  static targets = ["name"];
   connect() {
+    console.log("I am connected!!!");
   }
+
+  getUserName = () => {
+    const element = this.nameTarget;
+    const name = element.textContent;
+    alert(`You clicked on, ${name}`);
+  };
 }
