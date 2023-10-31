c051a76098c53fde576e62d0c6a855bfad541a3d
Robi <robi.maliqi@hotmail.co.uk>
2023-10-31 17:16:03 +0000

    WIP: Edit user description with modal not working, have to come back to it and fix it

--- app/javascript/controllers/bs_modal_controller.js
+++ app/javascript/controllers/bs_modal_controller.js
@@ -3,14 +3,17 @@ import { Controller } from "@hotwired/stimulus";
 // Connects to data-controller="bs-modal"
 export default class extends Controller {
   connect() {
+    console.log("I Am Also Connected!!!");
     this.modal = new bootstrap.Modal(this.element, {
       keyboard: false,
     });
-    thismodal.show();
+    this.modal.show();
   }
+
   disconnect() {
     this.modal.hide();
   }
+
   submitEnd(event) {
     this.modal.hide();
   }
--- app/javascript/controllers/edit_user_description_controller.js
+++ app/javascript/controllers/edit_user_description_controller.js
@@ -18,7 +18,7 @@ export default class extends Controller {
     this.url = this.element.getAttribute("href");
     fetch(this.url, {
       headers: {
-        Accept: "text/vnd.turbo-stream.html",
+        Accept: "text/vnd.turbo_stream.html",
       },
     })
       .then((response) => response.text())
