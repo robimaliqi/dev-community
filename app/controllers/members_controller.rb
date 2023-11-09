5b246c9783d0b5456e19d956865c747c7f1af251
Robi <robi.maliqi@hotmail.co.uk>
2023-11-09 21:16:16 +0000

    Update user personal details

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,5 +1,5 @@
 class MembersController < ApplicationController
-  before_action :authenticate_user!, only: %i[edit_description update_description]
+  before_action :authenticate_user!, only: %i[edit_description update_description edit_personal_details update_personal_details]
   def show
     @user = User.find(params[:id])
   end
@@ -12,5 +12,21 @@ class MembersController < ApplicationController
         format.turbo_stream { render turbo_stream: turbo_stream.replace('member-description', partial: 'members/member_description', locals: { user: current_user})}
       end
     end
+  end
+
+  def edit_personal_details; end
+
+  def update_personal_details
+    respond_to do |format|
+      if current_user.update(user_personal_info_params)
+        format.turbo_stream { render turbo_stream: turbo_stream.replace('member-personal-details', partial: 'members/member_personal_details', locals: { user: current_user})}
+      end
+    end
+  end
+
+  private 
+
+  def user_personal_info_params
+    params.require(:user).permit(:first_name, :last_name, :city, :county, :country, :postcode, :profile_title)
   end
 end
