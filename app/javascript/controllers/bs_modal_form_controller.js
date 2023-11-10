54541960e35244de074011b9d6b7f36e0c53d885
Robi <robi.maliqi@hotmail.co.uk>
2023-11-10 18:19:13 +0000

    Refactor stimulus controller

--- app/javascript/controllers/bs_modal_form_controller.js
+++ app/javascript/controllers/bs_modal_form_controller.js
@@ -1,16 +1,13 @@
 import { Controller } from "@hotwired/stimulus";
 
-// Connects to data-controller="edit-user-description"
+// Connects to data-controller="bs-modal-form"
 export default class extends Controller {
   connect() {
     console.log("I am connected!!");
   }
 
   initialize() {
-    this.element.setAttribute(
-      "data-action",
-      "click->edit-user-description#showModal"
-    );
+    this.element.setAttribute("data-action", "click->bs-modal-form#showModal");
   }
 
   showModal(event) {
