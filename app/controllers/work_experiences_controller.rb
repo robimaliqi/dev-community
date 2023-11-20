e08a68bea87868796c3b847804eca96505e07957
Robi <robi.maliqi@hotmail.co.uk>
2023-11-20 18:12:29 +0000

    Added create logic

--- app/controllers/work_experiences_controller.rb
+++ app/controllers/work_experiences_controller.rb
@@ -10,6 +10,14 @@ class WorkExperiencesController < ApplicationController
   end
 
   def create
+    @work_experience = current_user.work_experiences.new(work_experience_params)
+    respond_to do |format|
+      if @work_experience.save
+        format.turbo_stream { render turbo_stream: turbo_stream.append('work_experience_items', partial: 'work_experiences/work_experience', locals: { work_experience: @work_experience }) }
+      else
+        format.turbo_stream { render turbo_stream: turbo_stream.replace('remote_modal', partial: 'shared/turbo_modal', locals: { form_partial: 'work_experiences/form', modal_title: 'Add new work experience'})}
+      end
+    end
   end
 
   def update
@@ -25,6 +33,6 @@ class WorkExperiencesController < ApplicationController
   end
 
   def work_experience_params
-    params.require(:work_experience).permit(:start_date, :end_date, :currently_working_here, :company, :employemnt_type, :location, :location_type, :description, :user_id, :job_title)
+    params.require(:work_experience).permit(:start_date, :end_date, :currently_working_here, :company, :employment_type, :location, :location_type, :description, :user_id, :job_title)
   end
 end
