142d5b6d82e99f98296433a463761862470dc8ed
Robi <robi.maliqi@hotmail.co.uk>
2023-11-28 21:25:06 +0000

    Add assocations with connections and destroy method so when a user is deleted all assocations with that user are deleted

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable      
 
-  has_many :work_experiences     
+  has_many :work_experiences, dependent: :destroy
+  has_many :connections, dependent: :destroy    
 
   PROFILE_TITLE = [
     'Senior Ruby on Rails Developer',
