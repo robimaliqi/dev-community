9a528ac3532bd2942e18b595181244b9516ba886
Robi <robi.maliqi@hotmail.co.uk>
2023-12-03 21:31:31 +0000

    Fixed typo's and added error checks

--- app/javascript/controllers/connections_controller.js
+++ app/javascript/controllers/connections_controller.js
@@ -1,29 +1,39 @@
 import { Controller } from "@hotwired/stimulus";
 
-// Connects to data-controller="connections"
 export default class extends Controller {
   static targets = ["connection"];
-  connect() {}
 
-  initialize() {
+  connect() {
     this.element.setAttribute(
       "data-action",
       "click->connections#prepareConnectionParams"
     );
   }
 
-  prepareConnectionParams() {
+  prepareConnectionParams(event) {
     event.preventDefault();
-    this.url = this.element.getAttribute("href");
 
     const element = this.connectionTarget;
+
+    if (!element) {
+      console.error("Connection target not found.");
+      return;
+    }
+
     const requesterId = element.dataset.requesterId;
     const connectedId = element.dataset.connectedId;
+
+    if (!requesterId || !connectedId) {
+      console.error("Requester ID or Connected ID is missing.");
+      return;
+    }
+
+    this.url = this.element.getAttribute("href");
 
     const connectionBody = new FormData();
 
     connectionBody.append("connection[user_id]", requesterId);
-    connectionBody.append("connection[connected user id]", connectedId);
+    connectionBody.append("connection[connected_user_id]", connectedId); // Fixed typo in attribute name
     connectionBody.append("connection[status]", "pending");
 
     fetch(this.url, {
@@ -31,12 +41,15 @@ export default class extends Controller {
       headers: {
         Accept: "text/vnd.turbo-stream.html",
         "X-CSRF-Token": document
-          .querySelector('metal[name="csrf-token"]')
+          .querySelector('meta[name="csrf-token"]') // Fixed typo in 'metal' to 'meta'
           .getAttribute("content"),
       },
       body: connectionBody,
     })
       .then((response) => response.text())
-      .then((html) => Turbo.renderStreamMessage(html));
+      .then((html) => Turbo.renderStreamMessage(html))
+      .catch((error) => {
+        console.error("Error during fetch:", error);
+      });
   }
 }
