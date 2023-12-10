e19dd4d81687f449e02f12758ca796a9c800a8a0
Robi <robi.maliqi@hotmail.co.uk>
2023-12-10 09:53:00 +0000

    Add query to fetch user connection count

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,6 +2,7 @@ class MembersController < ApplicationController
   before_action :authenticate_user!, only: %i[edit_description update_description edit_personal_details update_personal_details]
   def show
     @user = User.find(params[:id])
+    @connections = Connection.where('user_id = ? OR connected_user_id = ?', params[:id], params[:id]).where(status: 'accepted')
   end
 
   def edit_description; end
