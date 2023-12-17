df67b549b5bfea2e1f1aa67af64cdea5ef6b671f
Robi <robi.maliqi@hotmail.co.uk>
2023-12-17 21:57:53 +0000

    Fixed Typo

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -64,14 +64,14 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
-  config.include Device::Test::ControllerHelpers, type: :controller
-  config.include Device::Test::IntegrationHelpers, type: :request
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::IntegrationHelpers, type: :request
 
   Capybara.register_driver :chrome do |app|
     Capybara::Selenium::Driver.new(app, browser: :chrome)
   end
 
   Capybara.javascript_driver = :chrome
-  Capybara.defailt_driver = :chrome
+  Capybara.default_driver = :chrome
 
 end
