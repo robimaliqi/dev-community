ff970d4db4537dfeaf0693b62a8ce5ff2532d2ee
Robi <robi.maliqi@hotmail.co.uk>
2023-12-18 21:54:55 +0000

    Add log in with email

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -27,5 +27,31 @@ RSpec.feature "Logins", type: :feature do
       expect(page).to have_text(user.name)
       expect(page).to have_text(user.profile_title)
     end
+
+    it 'should log in user with email' do
+      user = User.create(
+        email: 'user@example.com',
+        password: 'password',
+        username: 'test',
+        first_name: 'example',
+        last_name: 'any',
+        profile_title: 'Senior RoR developer',
+        confirmed_at: DateTime.now
+      )
+
+      visit root_path
+
+      fill_in 'user_login', with: user.email
+      fill_in 'user_password', with: user.password
+
+      click_button 'Log in'
+      expect(page).to have_text('Dev Community')
+      expect(page).to have_link('My Profile')
+      expect(page).to have_link('My Network')
+      expect(page).to have_link('Sign Out')
+      expect(page).to have_text('Search profesionals across the world!')
+      expect(page).to have_text(user.name)
+      expect(page).to have_text(user.profile_title)
+    end
   end
 end
