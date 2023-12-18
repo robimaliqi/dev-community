8c7e0c2eb60131af7c6f4a7039ad9907bbcbfb6e
Robi <robi.maliqi@hotmail.co.uk>
2023-12-18 21:37:16 +0000

    Fixed Typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,7 +37,7 @@ class User < ApplicationRecord
     if(login = conditions.delete(:login))
       where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { value: login.downcase }]).first
     elsif conditions.has_key?(:username) || conditions.has_key?(:email)
-      where(conditions.to_h).first_name
+      where(conditions.to_h).first
     end
   end
 
