703306574e369200efd735c1d14c066ecae6dd78
Robi <robi.maliqi@hotmail.co.uk>
2023-12-18 21:17:43 +0000

    Fixed typos

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,10 +33,10 @@ class User < ApplicationRecord
   end 
 
   def self.find_for_database_authentication(warden_conditions)
-    conditions = warren_conditions.dup
+    conditions = warden_conditions.dup
     if(login = conditions.delete(:login))
       where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { value: login.downcase }]).first
-    elsif conditons.has_key?(:username) || conditions.has_key?(:email)
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
       where(conditions.to_h).first_name
     end
   end
